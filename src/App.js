ada5be74947d441cca488e0e031079d6e53f22e2
Isaac Luy <isaacluy@github.com>
2021-06-22 14:20:20 -0400

    Adds TailwindUI <NavBar /> with search bar
    https://tailwindui.com/components/application-ui/navigation/navbars#component-ac4ed72c2e03976dc5415ce711fe2f78

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import NavBar from "./components/NavBar"
+
 const App = () => {
   return (
-    <div className="container mx-auto">
-      <h1 className="m-4 text-4xl text-center">Mi Librito Digital</h1>
+    <div>
+      <NavBar />
     </div>
   );
 }
