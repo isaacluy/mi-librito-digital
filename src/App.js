9319c98fd9454f9a830fb8c6870fcbac44ca55b4
Isaac Luy <isaacluy@github.com>
2021-06-22 14:11:58 -0400

    Resets create-react-app to a blank app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto">
+      <h1 className="m-4 text-4xl text-center">Mi Librito Digital</h1>
     </div>
   );
 }
