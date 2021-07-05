801d296058bb00b76164b335ef4d632a500efed7
Isaac Luy <isaacluy@github.com>
2021-07-05 16:23:24 -0400

    Refactors <Directory />

--- src/components/Directory.js
+++ src/components/Directory.js
@@ -5,48 +5,61 @@ import { useGroups } from "../data/AirtableDB";
 
 import { getItemURL } from "../utils/itemsUtils";
 
+const renderGroups = groups => {
+  const groupNames = Object.keys(groups);
+
+  return groupNames.map(groupName => renderGroup(groupName, groups));
+};
+
+const renderGroup = (groupName, groups) => {
+  return (
+    <div key={groupName} className="relative">
+      <div className="z-10 sticky top-0 border-t border-b border-gray-200 bg-gray-50 px-6 py-1 text-sm font-medium text-gray-500">
+        <h3>{groupName}</h3>
+      </div>
+      <ul className="relative z-0 divide-y divide-gray-200">
+        {renderItems(groups[groupName])}
+      </ul>
+    </div>
+  );
+};
+
+const renderItems = items => {
+  return items.map(item => renderItem(item));
+};
+
+const renderItem = item => {
+  return (
+    <li key={item.id} className="bg-white">
+      <div className="relative px-6 py-5 flex items-center space-x-3 hover:bg-gray-50 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
+        <div className="flex-shrink-0">{renderItemImage(item)}</div>
+        <div className="flex-1 min-w-0">{renderItemLink(item)}</div>
+      </div>
+    </li>
+  );
+};
+
+const renderItemImage = item => {
+  return <img className="h-10 w-10 rounded-full" src={item.imageUrl} alt="" />;
+};
+
+const renderItemLink = item => {
+  return (
+    <Link className="focus:outline-none" to={getItemURL(item.id, item.type)}>
+      {/* Extend touch target to entire panel */}
+      <span className="absolute inset-0" aria-hidden="true" />
+      <p className="text-sm font-medium text-gray-900">{item.title}</p>
+      <p className="text-sm text-gray-500 truncate">{item.subtitle}</p>
+    </Link>
+  );
+};
+
 const Directory = () => {
   const [isLoading, groups] = useGroups();
 
   return !isLoading ? (
     <nav className="h-full overflow-y-auto" aria-label="Directory">
-      {Object.keys(groups).map(groupName => (
-        <div key={groupName} className="relative">
-          <div className="z-10 sticky top-0 border-t border-b border-gray-200 bg-gray-50 px-6 py-1 text-sm font-medium text-gray-500">
-            <h3>{groupName}</h3>
-          </div>
-          <ul className="relative z-0 divide-y divide-gray-200">
-            {groups[groupName].map(item => (
-              <li key={item.id} className="bg-white">
-                <div className="relative px-6 py-5 flex items-center space-x-3 hover:bg-gray-50 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
-                  <div className="flex-shrink-0">
-                    <img
-                      className="h-10 w-10 rounded-full"
-                      src={item.imageUrl}
-                      alt=""
-                    />
-                  </div>
-                  <div className="flex-1 min-w-0">
-                    <Link
-                      className="focus:outline-none"
-                      to={getItemURL(item.id, item.type)}
-                    >
-                      {/* Extend touch target to entire panel */}
-                      <span className="absolute inset-0" aria-hidden="true" />
-                      <p className="text-sm font-medium text-gray-900">
-                        {item.title}
-                      </p>
-                      <p className="text-sm text-gray-500 truncate">
-                        {item.subtitle}
-                      </p>
-                    </Link>
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
+      {renderGroups(groups)}
     </nav>
   ) : null;
 };
