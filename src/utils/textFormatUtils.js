003a122425514f9382aeec8dc40831c2d5017c98
Isaac Luy <isaacluy@github.com>
2021-08-20 10:38:56 -0400

    Fixes a minor bug for splitAndRenderByDivider

--- src/utils/textFormatUtils.js
+++ src/utils/textFormatUtils.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 export const splitAndRenderByDivider = (text, divider = "*") => {
+  if (typeof text !== "string") {
+    return text;
+  }
+
   const phrases = text ? text.split(divider) : [];
 
   return phrases.map((phrase, index) => <p key={index}>{phrase}</p>);
