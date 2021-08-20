fb6d4d82b67383642060e179d0e10f0e8f2df76a
Isaac Luy <isaacluy@github.com>
2021-08-20 10:39:43 -0400

    Fixes the sanscrit formatting

--- src/components/Subtitle.js
+++ src/components/Subtitle.js
@@ -1,8 +1,12 @@
 import React from "react";
 
+import { splitAndRenderByDivider } from "../utils/textFormatUtils";
+
 const Subtitle = ({ subtitle }) => {
   return subtitle ? (
-    <h2 className=" text-center text-2xl md:text-3xl">{subtitle}</h2>
+    <div className=" text-center text-2xl md:text-3xl">
+      {splitAndRenderByDivider(subtitle)}
+    </div>
   ) : null;
 };
 
